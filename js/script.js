3ef693baffc63b40bfe188e705836adbfa165911
XID404 <36547811+XID404@users.noreply.github.com>
2026-09-15 18:37:06 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 
+const audio = document.getElementById('audioElement');
+const playPauseBtn = document.getElementById('playPauseBtn');
+let isPlaying = false;
+
+playPauseBtn.addEventListener('click', () => {
+    if (isPlaying) {
+        audio.pause();
+        playPauseBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
+    } else {
+        audio.play();
+        playPauseBtn.innerHTML = '<i class="fa-solid fa-pause"></i>';
+    }
+    isPlaying = !isPlaying;
+});
